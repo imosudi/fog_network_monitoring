466e6934bda365d2951c4ad142d0c50104b1c07e
Mosudi I. O <imosudi@gmail.com>
2025-07-10 02:21:20 +0100

    updates

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 # --- Launch Enhanced Real-Time Simulation ---
-#from dashboard.ema_dashboard import EMALiveMultiNodeDashboard
 from dashboard.ema_dashboard_dataset import EMALiveMultiNodeDashboard
-from simulation.data_generator import generate_comprehensive_dataset
+from utils.node_id_extractor import extract_node_ids, extract_node_ids_from_string
 
 
 if __name__ == "__main__":
-    
-    # Generate comprehensive dataset
-    dataset = generate_comprehensive_dataset(num_iterations=50)
-    
     # Create a diverse set of nodes for testing
+    node_ids = extract_node_ids('data/node_list.csv')
+    print("node_ids: ", node_ids)
     sample_node_ids = [
         "L1N_01",
         "L2N_01", "L2N_02", "L2N_03", "L2N_04",
@@ -26,5 +23,8 @@ if __name__ == "__main__":
 
     
     
-    dashboard = EMALiveMultiNodeDashboard(sample_node_ids, max_display=len(sample_node_ids))
-    ani = dashboard.run()
+    #dashboard = EMALiveMultiNodeDashboard(sample_node_ids, max_display=len(sample_node_ids))
+    dashboard = EMALiveMultiNodeDashboard(node_ids, max_display=len(node_ids))
+    dashboard.run()
+    dashboard.save_metrics_to_csv("data/node_metrics_data.csv")
+    #ani = dashboard.run()
